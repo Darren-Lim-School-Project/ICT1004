d0ccb700f82a384edfb79ab3eddaaf301f1b7fa7
darrennnnnlim <darrenlimqinyi@hotmail.com>
2020-11-02 22:15:58 +0800

    (+) Skeleton codes (+) Header (+) Navigation (+) Footer (+) Bootstrap 4.5.3 (+) JQuery 3.5.1

---
+++ foot.inc.php
@@ -0,0 +1,3 @@
+<footer class="container">
+    <p>Copyright &copy; 2020 ICT 1004 - Group 4</p>
+</footer>
--- index.php
+++ index.php
@@ -4,14 +4,26 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<html>
+<html lang="en">
+<!-- 
+Declare lang attribute not only declare the language of web page, it can also assist search engines and browser
+-->
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+        <?php
+            include "head.inc.php";
+        ?>
     </head>
     <body>
         <?php
-        // put your code here
+            include "nav.inc.php";
+        ?>
+        
+        <main class="container">
+            <h1> Information here </h1>
+        </main>
+        
+        <?php
+            include "foot.inc.php";
         ?>
     </body>
 </html>
