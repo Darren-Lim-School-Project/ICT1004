cdb347a01ae354e2290ddd7d6415157e245a1aa4
exercutor5 <lim_yue_heng@hotmail.com>
2020-11-13 09:27:14 +0800

    To edit further

--- process/post_comment.php
+++ process/post_comment.php
@@ -1,8 +1,20 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+require_once ("db.php");
+$commentId = isset($_POST['comment_id']) ? $_POST['comment_id'] : "";
+$comment = isset($_POST['comment']) ? $_POST['comment'] : "";
+$commentSenderName = isset($_POST['name']) ? $_POST['name'] : "";
+$date = date('Y-m-d H:i:s');
 
+$sql = "INSERT INTO tbl_comment(parent_comment_id,comment,comment_sender_name,date) VALUES ('" . $commentId . "','" . $comment . "','" . $commentSenderName . "','" . $date . "')";
+
+$result = mysqli_query($conn, $sql);
+
+if (!$result) {
+    $result = mysqli_error($conn);
+}
+echo $result;
+
+
+
+?>
