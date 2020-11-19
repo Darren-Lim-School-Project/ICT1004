9c73779051449f892faa587deaf3847ab25c29c9
stellar <stellar@DESKTOP-6727DC9>
2020-11-20 00:17:36 +0800

    process login back to home button direct to gui.php added db.php trying to retrieve

--- contactUs.php
+++ contactUs.php
@@ -9,18 +9,15 @@
     ?>
 
     <!-- !PAGE CONTENT! -->
-    <span class="w3-button w3-hide-large w3-xxlarge w3-hover-text-grey" onclick="w3_open()"><i class="fa fa-bars"></i></span>
-    <!--side button-->
+   <span class="w3-button w3-hide-large w3-xxlarge w3-hover-text-grey" onclick="w3_open()"><i class="fa fa-bars"></i></span>
+  <!--side button-->
     <div class="w3-main" style="margin-left:300px">
         <div class="w3-container w3-padding-64  w3-light-blue w3-grayscale-min" id="us">
             <div class="w3-content">
                 <h1 class="w3-center w3-text-grey"><b>contact us </b></h1>
 
                 <?php
-                $config = parse_ini_file('../../../private/dbconfig.ini');
-                $conn = new mysqli($config['servername'], $config['username'],
-                        $config['password'], $config['dbname']);
-                $stmt = $conn->prepare("SELECT * FROM accounts WHERE email=ss@ss.com");
+               
                 ?>
 
                 <!-- Contact Section -->
@@ -30,14 +27,8 @@
                         <p><input class="w3-input w3-padding-16 w3-border" type="text" placeholder="Name" required name="Name"></p>
 
  <!-- <input type='email' id='email' name='email' value=<?php echo $_SESSION['email']; ?> -->
-                        0
-
-                       
-                        echo"<tr>";
-                            echo"<td><input type=\"text\" name=\"user_id\" value=\".$row['email']."\"></td>";
-                            echo"</tr>";
-                        }
-
+                        
+                        
                         <p><input class="w3-input w3-padding-16 w3-border" type="text" placeholder= $_SESSION['email'] required name="Email"></p>
                         <p><input class="w3-input w3-padding-16 w3-border" type="text" placeholder="Subject" required name="Subject"></p>
                         <p><input class="w3-input w3-padding-16 w3-border" type="text" placeholder="Message" required name="Message"></p>
