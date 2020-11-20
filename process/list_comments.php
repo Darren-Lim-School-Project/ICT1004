2d2bd626f9bf67af25369220e5ddbb7d5b8f8af2
exercutor5 <lim_yue_heng@hotmail.com>
2020-11-20 21:03:35 +0800

    wip

--- process/list_comments.php
+++ process/list_comments.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once ("db.php");
+$config = parse_ini_file('../../../private/dbconfig.ini');
 
-$sql = "SELECT * FROM tbl_comment ORDER BY parent_comment_id asc, comment_id asc";
+$sql = "SELECT * FROM comment WHERE post_id = " . $post_id . ", ORDER BY, comment_id asc";
 
 $result = mysqli_query($conn, $sql);
 $record_set = array();
--- process/post_comment.php
+++ process/post_comment.php
@@ -5,9 +5,9 @@ $config = parse_ini_file('../../../private/dbconfig.ini');
 $commentId = isset($_POST['comment_id']) ? $_POST['comment_id'] : "";
 $comment = isset($_POST['comment']) ? $_POST['comment'] : "";
 $commentSenderName = isset($_POST['name']) ? $_POST['name'] : "";
-$date = date('Y-m-d H:i:s');
+// $date = date('Y-m-d H:i:s');
 
-$sql = "INSERT INTO tbl_comment(parent_comment_id,comment,comment_sender_name,date) VALUES ('" . $commentId . "','" . $comment . "','" . $commentSenderName . "','" . $date . "')";
+$sql = "INSERT INTO comment(parent_comment_id,comment,comment_sender_name) VALUES ('" . $commentId . "','" . $comment . "','" . $commentSenderName . "')";
 
 $result = mysqli_query($conn, $sql);
 
