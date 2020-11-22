da5c5626782b370ead9d9b6abdbbf0b4fc2d8e39
0122syh <0122syh@gmail.com>
2020-11-22 13:00:34 +0800

    Updated nav and process login

--- nav.inc.php
+++ nav.inc.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-sm navbar-light bg-light">
     <a class="navbar-brand" href="userMain.php">
-        <img src="images/Logo_Simplegram.png" alt="LOGO" width="200"/>
+        <img src="images/sp.jpg" alt="LOGO" width="200"/>
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -9,7 +9,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ml-auto">
             <li class="nav-item">
-                <a class="nav-link" href="upload.php"><img src="images/baseline_publish_black_18dp.png" alt="Upload"/></a>
+                <a class="nav-link" href="upload.php">Upload</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="gui.php"><?php echo $_SESSION['fname'] . " " . $_SESSION['lname']; ?> </a>
