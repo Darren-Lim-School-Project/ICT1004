376369646e0ca1900f9ad56df872b3006e385e75
0122syh <0122syh@gmail.com>
2020-11-25 23:42:16 +0800

    Remove Like function

--- process/connect.php
+++
@@ -1,4 +0,0 @@
-<?php
-mysql_connect('localhost', 'simplesqldev', 'password') or die(mysql_error());
-mysql_select_db('SimpleGram');
-?>
--- process/init.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-
-include 'image.php';
-include 'like.php';
-include 'connect.php';
-?>
