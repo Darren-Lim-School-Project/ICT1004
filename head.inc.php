d33fe4508d45049420d9843e59f79d23cbfb07ab
darrennnnnlim <darrenlimqinyi@hotmail.com>
2020-11-28 05:01:00 +0800

    (+) Fixed head.inc.php, previously was accidentally removed for unknown reason

--- head.inc.php
+++ head.inc.php
@@ -1,14 +1,6 @@
-<!--jQuery-->
-<script defer
-        src="https://code.jquery.com/jquery-3.5.1.slim.min.js" 
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-        crossorigin="anonymous">
-</script>
-
-<!--Ajax-->
-<script defer 
-        src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js">
-</script>
+<title>SimpleGram</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 <link rel="stylesheet"    
       href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" 
@@ -26,6 +18,17 @@
 <link rel="stylesheet" 
       href="css/main.css">
 
+<!--Ajax-->
+<script defer 
+        src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js">
+</script>
+
+<!--jQuery-->
+<script defer
+        src="https://code.jquery.com/jquery-3.5.1.slim.min.js" 
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous">
+</script>
 <!--Bootstrap JS-->
 <script defer
         src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" 
