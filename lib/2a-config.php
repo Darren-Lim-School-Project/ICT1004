e749e0da9031abf8c6289d169735ad24b451a848
exercutor5 <lim_yue_heng@hotmail.com>
2020-11-29 00:39:53 +0800

    Changed comment system database config to parse ini file

--- lib/2a-config.php
+++ lib/2a-config.php
@@ -8,9 +8,12 @@ define('PATH_LIB', __DIR__ . DIRECTORY_SEPARATOR);
 
 // DATABASE SETTINGS
 // ! CHANGE THESE TO YOUR OWN !
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'SimpleGram');
+
+$config = parse_ini_file('../../private/dbconfig.ini');
+
+define('DB_HOST', $config['servername']);
+define('DB_NAME', $config['dbname']);
 define('DB_CHARSET', 'utf8');
-define('DB_USER', 'simplesqldev');
-define('DB_PASSWORD', 'password');
+define('DB_USER', $config['username']);
+define('DB_PASSWORD', $config['password']);
 ?>
